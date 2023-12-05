447250458f887c29b4a3c8a53981d6494f8e879c
mathewjordan <mat@northwestern.edu>
2023-12-05 01:02:47 +0000

    Deploying to gh-pages from @ canopy-iiif/example-anatomia@a1dae54c34aa2c0e6158023a61f948fc06c26830 🚀

--- _next/static/chunks/881.ddaddc01f7adc0a3.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[881],{8881:function(a){a.exports=JSON.parse('{"collection":"https://raw.githubusercontent.com/canopy-iiif/example-anatomia/main/public/anatomia.json","metadata":["dateIssued"],"label":{"none":["Anatatomia"]},"summary":{"none":["Anatomia Collection: anatomical plates 1522-1867 Navigation menu"]}}')}}]);
